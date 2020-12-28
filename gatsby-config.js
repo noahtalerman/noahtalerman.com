c256b6b2c955e04574c39f51cad57b6ceb82c96f
noahtalerman <47070608+noahtalerman@users.noreply.github.com>
2020-12-27 16:20:16 -0800

    Remove excess pages/components. Cleanup index.js. (#1)
    Clean up the unneeded pages, components, styles from Gatsby boilerplate.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Noah Talerman`,
+    description: `22 year old software geek, Arsenal fan, and Rocket League player.`,
+    author: `Noah Talerman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
