4072e8c280685409feae8e6cd33e87e9b4a7b515
Noah Talerman <noahtal@umich.edu>
2021-01-02 10:40:22 -0800

    Add Cera CY and Spectral fonts. Figured out how to configure tailwind for new fonts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,13 @@ module.exports = {
     author: `Noah Talerman`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,9 +21,12 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,19 +23,19 @@ const IndexPage = () => {
       <div className="col-start-4 col-span-3">
         <h1 className="text-xl">Noah Talerman</h1>
         <p>During the week I’m working on product at fleetdm.com and during the other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC dissapoint.</p>
-        <h2 className="font-bold">Work I'm proud of</h2>
+        <h2 className="text-xl">Work I'm proud of</h2>
         <div className="flex">
-          <Link to="https://github.com/fleetdm/fleet" className="underline">Fleet Device Management</Link>
+          <a to="https://github.com/fleetdm/fleet" className="">Fleet Device Management</a>
           <span>github.com/fleetdm/fleet</span>
         </div>
         <p>My proud gig. I can be doing a lot of things related to “product” on any given day. These things are wireframes, communicating wth users, bug fixes, front end code, community support, public facing documentation, roadmap management</p>
         <div className="flex">
-          <a href="https://github.com/snordale/music-app" className="underline">The Ivy Platform</a>
+          <a href="https://github.com/snordale/music-app" className="">The Ivy Platform</a>
           <span>github.com/snordale/music-app</span>
         </div>
         <p>Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision. Using cool tech like Django, Spotify’s API, and React Native....</p>
-        <h2 className="font-bold">Songs I'm into</h2>
-        <h2 className="font-bold">Pictures I took</h2>
+        <h2 className="text-xl">Songs I'm into</h2>
+        <h2 className="text-xl">Pictures I took</h2>
       </div>
     </Layout>
   );
