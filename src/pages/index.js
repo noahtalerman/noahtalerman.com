8eaede41d73c7183ec589d0612aa185b84044df8
Noah Talerman <noahtal@umich.edu>
2021-01-02 16:53:15 -0800

    New light Spectral font.

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,21 +23,21 @@ const IndexPage = () => {
       <div className="col-start-3 col-span-5">
         <h1>NOAH TALERMAN'S SITE</h1>
         <h2>A SPACE TO SHARE MY FAVORITE WORK AND MEDIA</h2>
-        <p className="mt-lg w-1/2">Welcome to my site</p>
-        <p className="mt-md w-1/2">During the week I’m working on product at fleetdm.com and during the other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC dissapoint.</p>
+        <p className="mt-lg lg:w-1/2">Welcome to my site</p>
+        <p className="mt-md lg:w-1/2">During the week I’m working on product at fleetdm.com and during the other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC dissapoint.</p>
         <img className="w-1/2"></img>
         <h3 className="mt-xl">WORK I'M PROUD OF</h3>
         <div className="flex justify-between mt-lg">
           <a to="https://github.com/fleetdm/fleet">Fleet Device Management</a>
-          <span className="font-display not-italic">github.com/fleetdm/fleet</span>
+          <span className="font-display not-italic hidden lg:inline">github.com/fleetdm/fleet</span>
         </div>
         <p className="mt-md">My proud gig. I can be doing a lot of things related to “product” on any given day. These things are wireframes, communicating wth users, bug fixes, front end code, community support, public facing documentation, roadmap management</p>
         <div className="flex justify-between mt-lg">
           <a href="https://github.com/snordale/music-app">The Ivy Platform</a>
-          <span className="font-display not-italic">github.com/snordale/music-app</span>
+          <span className="font-display not-italic hidden lg:inline">github.com/snordale/music-app</span>
         </div>
         <p className="mt-md">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision. Using cool tech like Django, Spotify’s API, and React Native....</p>
-        <h3 className="mt-xl">MY LAST 3 LISTENS ON SPOTIFY</h3>
+        {/* <h3 className="mt-xl">MY LAST 3 LISTENS ON SPOTIFY</h3>
         <table className="table-auto w-full text-left mt-lg">
           <thead className="font-display not-italic">
             <tr>
@@ -52,7 +52,7 @@ const IndexPage = () => {
             <tr></tr>
           </tbody>
         </table>
-        <h3 className="mt-xl">PICTURES I TOOK</h3>
+        <h3 className="mt-xl">PICTURES I TOOK</h3> */}
       </div>
     </Layout>
   );
