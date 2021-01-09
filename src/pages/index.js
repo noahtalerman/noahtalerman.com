9fbb8c82961ff34a065b19c6f76ebdf7d84eb750
Noah Talerman <noahtal@umich.edu>
2021-01-09 15:59:25 -0800

    Add sketch asset

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { useStaticQuery, graphql } from "gatsby";
 
-import meImg from "../assets/images/me.jpg";
+import meImg from "../assets/images/emma-sketch-pen.jpeg";
 
 const IndexPage = () => {
 
@@ -26,25 +26,25 @@ const IndexPage = () => {
         <h2>A SPACE TO SHARE MY FAVORITE WORK AND MEDIA</h2>
         <div className="flex justify-between align-center mt-xl">
           <div>
-            <p className="mt-lg">Welcome to my site</p>
-            <p className="mt-md">During the week I’m working on product at fleetdm.com and during the other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC dissapoint.</p>
+            <p>Welcome to my site</p>
+            <p className="mt-md">During the week I’m working on product at Fleet. Other days, I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC.</p>
           </div>
           <div className="lg:flex lg:justify-end hidden">
-            <img src={meImg} style={{maxWidth: "240px"}}className="object-cover h-auto ml-xl"></img>
+            <img src={meImg} style={{maxWidth: "180px"}}className="object-cover h-auto ml-xl"></img>
           </div> 
         </div>
         <h3 className="mt-xl">WORK I'M PROUD OF</h3>
-        <div className="flex justify-between mt-lg flex-col lg:flex-row">
+        <div className="flex justify-between items-start mt-lg flex-col lg:flex-row">
           <span className="font-display not-italic">Fleet Device Management</span>
           <a href="https://github.com/fleetdm/fleet" target="_blank" rel="noopener noreferrer" className="">View on GitHub</a>
         </div>
-        <p className="mt-md">My proud gig. I can be doing a lot of things related to “product” on any given day. These things are wireframes, communicating wth users, bug fixes, front end code, community support, public facing documentation, roadmap management</p>
-        <div className="flex justify-between mt-lg flex-col lg:flex-row">
+        <p className="mt-md">My proud gig. I can be doing a lot of things related to “product” on any given day.</p>
+        <div className="flex justify-between items-start mt-lg flex-col lg:flex-row">
           <span className="font-display not-italic">The Ivy Platform</span>
           <a href="https://github.com/snordale/music-app" target="_blank" rel="noopener noreferrer" className="">View on GitHub</a>
         </div>
-        <p className="mt-md">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision. Using cool tech like Django, Spotify’s API, and React Native....</p>
-        <div className="flex justify-between mt-lg flex-col lg:flex-row">
+        <p className="mt-md">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision.</p>
+        <div className="flex justify-between items-start mt-lg flex-col lg:flex-row">
           <span className="font-display not-italic">Computer Adoption and Income Inequality</span>
           <a href="https://columbiaeconreview.com/post/5f45c9c8be5493000729ed15" target="_blank" rel="noopener noreferrer" className="">View article</a>
         </div>
