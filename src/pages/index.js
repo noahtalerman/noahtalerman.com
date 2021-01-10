d649a7a7cf0e5cc13d7ad69031f356d654a8079c
Noah Talerman <noahtal@umich.edu>
2021-01-09 16:54:27 -0800

    Add Merriweather and Poppins font. Adjust styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,33 +22,33 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Noah Talerman's Site" />
       <div className="col-start-2 col-span-7 md:col-start-3 md:col-span-5">
-        <h1>NOAH TALERMAN'S SITE</h1>
-        <h2>A SPACE TO SHARE MY FAVORITE WORK AND MEDIA</h2>
-        <div className="flex justify-between align-center mt-xl">
+        <h1>Noah Talerman's site</h1>
+        <h2>A space to share my favorite work and media</h2>
+        <div className="flex justify-between items-center mt-xl">
           <div>
-            <p className="mt-lg">Welcome to my site</p>
-            <p className="mt-md">During the week I’m working on product at fleetdm.com and during the other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC dissapoint.</p>
+            <p className="text-light-text-grey dark:text-dark-text-grey">My day to day,</p>
+            <p className="mt-md text-light-text-grey dark:text-dark-text-grey">During the week I’m working on product at fleetdm.com. Other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC.</p>
           </div>
           <div className="lg:flex lg:justify-end hidden">
-            <img src={meImg} style={{maxWidth: "240px"}}className="object-cover h-auto ml-xl"></img>
+            <img src={meImg} style={{width: "200px", height: "200px", borderRadius: "4px"}}className="object-cover h-auto ml-xl"></img>
           </div> 
         </div>
-        <h3 className="mt-xl">WORK I'M PROUD OF</h3>
+        <h3 className="mt-xl">Work I'm proud of</h3>
         <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display not-italic">Fleet Device Management</span>
-          <a href="https://github.com/fleetdm/fleet" target="_blank" rel="noopener noreferrer" className="">View on GitHub</a>
+          <span className="font-display text-md">Fleet Device Management</span>
+          <a href="https://github.com/fleetdm/fleet" target="_blank" rel="noopener noreferrer" className="text-md">View on GitHub</a>
         </div>
-        <p className="mt-md">My proud gig. I can be doing a lot of things related to “product” on any given day. These things are wireframes, communicating wth users, bug fixes, front end code, community support, public facing documentation, roadmap management</p>
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">My proud gig. I can be doing a lot of things related to “product” on any given day.</p>
         <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display not-italic">The Ivy Platform</span>
-          <a href="https://github.com/snordale/music-app" target="_blank" rel="noopener noreferrer" className="">View on GitHub</a>
+          <span className="font-display text-md">The Ivy Platform</span>
+          <a href="https://github.com/snordale/music-app" target="_blank" rel="noopener noreferrer" className="text-md">View on GitHub</a>
         </div>
-        <p className="mt-md">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision. Using cool tech like Django, Spotify’s API, and React Native....</p>
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision.</p>
         <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display not-italic">Computer Adoption and Income Inequality</span>
-          <a href="https://columbiaeconreview.com/post/5f45c9c8be5493000729ed15" target="_blank" rel="noopener noreferrer" className="">View article</a>
+          <span className="font-display text-md">Computer Adoption and Income Inequality</span>
+          <a href="https://columbiaeconreview.com/post/5f45c9c8be5493000729ed15" target="_blank" rel="noopener noreferrer" className="text-md">Go to article</a>
         </div>
-        <p className="mt-md">Economics article that examines the relationship between income inequality and computer technology adoption.</p>
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">Economics article that examines the relationship between income inequality and computer technology adoption.</p>
         {/* <h3 className="mt-xl">MY LAST 3 LISTENS ON SPOTIFY</h3>
         <table className="table-auto w-full text-left mt-lg">
           <thead className="font-display not-italic">
