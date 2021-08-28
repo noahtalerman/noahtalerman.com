bda4e4b5eb25c40f0003df99c2104e045cb43cf4
Noah Talerman <noahtal@umich.edu>
2021-08-28 00:56:37 -0400

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,69 +2,69 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { useStaticQuery, graphql } from "gatsby";
 
 import meImg from "../assets/images/me.jpg";
 
 const IndexPage = () => {
 
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <Layout>
-      <SEO title="Noah Talerman's Site" />
+      <SEO title="Noah Talerman's internet profile" />
       <div className="col-start-2 col-span-7 md:col-start-3 md:col-span-5">
-        <h1>Noah Talerman's site</h1>
-        <h2>A space to share my favorite work and media</h2>
-        <div className="flex justify-between items-center mt-xl">
-          <div>
-            <p className="text-light-text-grey dark:text-dark-text-grey">My day to day,</p>
-            <p className="mt-md text-light-text-grey dark:text-dark-text-grey">During the week I’m working on product at fleetdm.com. Other days I’m running on Mercer Island, playing Rocket League, and watching Arsenal FC.</p>
-          </div>
-          <div className="lg:flex lg:justify-end hidden">
-            <img src={meImg} style={{maxWidth: "200px", height: "200px", borderRadius: "4px"}}className="object-cover h-auto ml-xl"></img>
-          </div> 
+        <div className="xl:flex flex-col xl:flex-row items-center">
+          <h1 className="mr-md mb-sm">Noah Talerman's internet profile</h1>
+          <img src={meImg} style={{maxWidth: "80px", height: "80px", borderRadius: "100%"}}className="object-cover h-auto" />
         </div>
+        <p className="mt-lg text-light-text-grey dark:text-dark-text-grey">
+          He's a 23 year old software newbie and behavioral economist wannabe.
+        </p>
+        <h3 className="mt-xl">My day to day</h3>
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">
+          <span>During the week I’m working on product at</span>
+          &nbsp;
+          <a href="https://fleetdm.com" target="_blank" rel="noreferrer">
+            fleetdm.com
+          </a>
+          <span>. Other days I’m running in </span>
+          <a href="https://www.google.com/maps/place/Prospect+Park/@40.6667184,-73.9730757,14z/data=!4m5!3m4!1s0x89c25b1087680443:0x178bf7c600df7a98!8m2!3d40.6602037!4d-73.9689558" target="_blank" rel="noreferrer"
+          >
+            Prospect Park, NY
+          </a>
+          , and watching
+          &nbsp;
+          <a href="https://twitter.com/Arsenal" target="_blank" rel="noreferrer" className="">
+            Arsenal Football Club
+          </a>
+          .
+        </p>
         <h3 className="mt-xl">Work I'm proud of</h3>
         <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display text-md">Fleet Device Management</span>
-          <a href="https://github.com/fleetdm/fleet" target="_blank" rel="noopener noreferrer" className="text-md">View on GitHub</a>
+          <a href="https://github.com/fleetdm/fleet/graphs/contributors" target="_blank" rel="noreferrer">
+            FleetDM
+          </a>
         </div>
-        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">My proud gig. I can be doing a lot of things related to “product” on any given day.</p>
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">My proud job. I can be doing a lot of things related to “product” on any given day.</p>
         <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display text-md">The Ivy Platform</span>
-          <a href="https://github.com/snordale/music-app" target="_blank" rel="noopener noreferrer" className="text-md">View on GitHub</a>
+          <a href="https://theivyplatform.com" target="_blank" rel="noreferrer">
+            The Ivy Platform
+          </a>
         </div>
-        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">Dope team effort with Sam Nordale and Eric Kim. Working to achieve our music sharing vision.</p>
-        <div className="flex justify-between mt-lg flex-col lg:flex-row">
-          <span className="font-display text-md">Computer Adoption and Income Inequality</span>
-          <a href="https://columbiaeconreview.com/post/5f45c9c8be5493000729ed15" target="_blank" rel="noopener noreferrer" className="text-md">Go to article</a>
-        </div>
-        <p className="mt-md text-light-text-grey dark:text-dark-text-grey">Economics article that examines the relationship between income inequality and computer technology adoption.</p>
-        {/* <h3 className="mt-xl">MY LAST 3 LISTENS ON SPOTIFY</h3>
-        <table className="table-auto w-full text-left mt-lg">
-          <thead className="font-display not-italic">
-            <tr>
-              <th className="w-1/2">SONG</th>
-              <th>ARTIST</th>
-              <th>ALBUM</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr></tr>
-            <tr></tr>
-            <tr></tr>
-          </tbody>
-        </table>
-        <h3 className="mt-xl">PICTURES I TOOK</h3> */}
+        <p className="mt-md text-light-text-grey dark:text-dark-text-grey"> 
+        <span>Shoutout</span>
+        &nbsp;
+        <a href="https://www.linkedin.com/in/samnordale/" target="_blank" rel="noreferrer">
+          Sam Nordale - CEO of everything
+        </a>
+        &nbsp;
+        <span>and</span> &nbsp;
+        <a href="https://www.linkedin.com/in/eric-kim-product-designer/" target="_blank" rel="noreferrer">
+          Eric Kim - Product designer
+        </a>
+        &nbsp;
+        <span>
+          for working to achieve the music sharing vision. I did UI design and frontend eng for the MVP.
+        </span>
+        </p>
       </div>
     </Layout>
   );
